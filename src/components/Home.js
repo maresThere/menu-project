2955d3e17e3e6a4801bb0a8362448fcc6c76f4ac
Marianne Kelly <marianne_kelly@hotmail.com>
2018-06-25 12:31:58 -0400

    small edits

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
     <h1>
       DINE
     </h1>
-    <p>Dine app is a practice in connecting and styling data files into React modules. Graphcool open-source framework was used to store and serve menu items. js files hold arrays of objects containg location data. </p>
+    <p>Dine app is a practice in connecting and styling data files into React modules. Graphcool open-source framework was used to store and serve menu items. js files hold arrays of objects containing location data. </p>
     <p>css grid was used to style the menu items. A combination of flexbox and grid were used for the location items. Styling is simple, clean and ideally for mobile viewing. </p>
   </div>
 )
